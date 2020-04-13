3b0cebc05913c8c65b0f01ed560abf78113dbead
kumarahul98 <kumar.rahul98k@gmail.com>
2020-04-13 21:26:27 +0000

    Changes to be committed: 	new file:   instance.tf 	new file:   provider.tf 	new file:   script.sh 	new file:   vars.tf added demo2 create vm with security group open port 22 for ssh

---
+++ demo2/instance.tf
@@ -0,0 +1,51 @@
+resource "aws_key_pair" "rahulsKey"{
+	key_name = "rahulsKey"
+	public_key = file(var.PUBLIC_KEY)
+}
+
+resource "aws_security_group" "terraform-sg-ssh" {
+  name        = "terraform-sg-ssh"
+  description = "Allow TLS inbound traffic"
+ # vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "test-var"{
+	ami = lookup(var.AMIS, var.AWS_REGION)
+	instance_type = "t2.micro"
+	key_name = aws_key_pair.rahulsKey.key_name
+
+	provisioner "file"{
+		source = "script.sh"
+		destination = "/tmp/script.sh"
+	}
+	provisioner "remote-exec"{
+		inline = [
+			"chmod +x /tmp/script.sh",
+			"sudo /tmp/script.sh"
+		]
+	}
+	connection{
+		 host = coalesce(self.public_ip, self.private_ip)
+		 type = "ssh"
+		     
+		user = var.INSTANCE_USERNAME
+		private_key = file(var.PRIVATE_KEY)
+	}
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.terraform-sg-ssh.id
+  network_interface_id = aws_instance.test-var.primary_network_interface_id
+}
+ 
